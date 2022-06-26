a0b8a7b5e8ad4ce4993a90d97de6551bd491c985
MOHAMMAD FAISAL <mdfaisalrndm@gmail.com>
2022-06-26 19:47:15 +0530

    Final

---
+++ client/src/components/Sidebar.js
@@ -0,0 +1,19 @@
+import { Stack } from "@mui/material";
+// import { Box } from "@mui/system";
+// import { getPosts } from "../api/posts";
+// import { isLoggedIn } from "../helpers/authHelper";
+import Footer from "./Footer";
+// import Loading from "./Loading";
+// import PostCard from "./PostCard";
+import TopPosts from "./TopPosts";
+
+const Sidebar = () => {
+  return (
+    <Stack spacing={2}>
+      <TopPosts />
+      <Footer />
+    </Stack>
+  );
+};
+
+export default Sidebar;
